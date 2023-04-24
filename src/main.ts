ee192af6b47430c873c8a58a959eba4e8a19566f
EdwardMelendezM <192666@unsaac.edu.pe>
2023-04-23 21:57:05 -0500

    adding create new item for Todo

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,61 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+interface Todo {
+  id: string
+  text: string
+}
+
+
+const listaTodo: Array<Todo> = [
+  {
+    id: crypto.randomUUID(),
+    text: "Cocinar"
+  },
+  {
+    id: crypto.randomUUID(),
+    text: "Estudiar"
+  }
+]
+let todoString = ''
+const Todos = listaTodo.map(item => (
+  todoString = todoString + `<div>${item.text}</div>`
+))
+
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <h1>Lista todo</h1>
+    <form id="form-input">
+      <input id="input-text" placeholder="Dormir"/>
+    </form>
+    <div id="body-text">
+      ${todoString}
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const formInput = document.querySelector("#form-input")!
+
+formInput.addEventListener("submit", (e) => {
+  e.preventDefault()
+  let inputText = document.querySelector<HTMLInputElement>("#input-text")!
+  if (inputText.value.trim().length < 2) return
+
+  listaTodo.push(
+    {
+      id: crypto.randomUUID(),
+      text: inputText.value
+    }
+  )
+  updateTodo()
+})
+
+function updateTodo() {
+  const bodyText = document.querySelector<HTMLDivElement>("#body-text")!
+  bodyText.innerHTML = ''
+  let todoString = ''
+  listaTodo.map(item => (
+    todoString = todoString + `<div>${item.text}</div>`
+  ))
+  bodyText.innerHTML = todoString
+}
